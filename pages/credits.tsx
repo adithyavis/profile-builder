9d401125ac91a4199d0f78ffb8d6a9a6d1f140ff
adithya_viswamithiran <adithya_viswamithiran@m-kiva.elecom>
2022-09-25 17:47:02 +0900

    add svgr and uuid packages

---
+++ pages/credits.tsx
@@ -0,0 +1,12 @@
+const Credits = () => {
+  return (
+    <>
+      <a href="https://www.freepik.com/free-vector/search-concept-yellow-folder-magnifier-icons-hand-drawn-cartoon-art-illustration_18508166.htm#query=empty%20file&position=1&from_view=search">
+        Image by mamewmy
+      </a>
+      on Freepik
+    </>
+  )
+}
+
+export default Credits
