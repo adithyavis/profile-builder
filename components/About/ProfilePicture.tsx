3009831681eee1d675adff958f7e4e8b289dba42
adithya_viswamithiran <adithya_viswamithiran@m-kiva.elecom>
2022-09-26 01:43:12 +0900

    logic  for uploading profile pic

--- components/About/ProfilePicture.tsx
+++ components/About/ProfilePicture.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import { Box, Image, Flex, Spacer, Button } from '@chakra-ui/react'
+import { Box, Image, Flex, Spacer, Button, Input } from '@chakra-ui/react'
 import { EditIcon } from '@chakra-ui/icons'
 import { useStoreContext } from 'providers/StoreProvider'
+import { createProfilePic } from 'domain/ProfilePic'
 
 export const ProfilePicture: React.FC = () => {
-  const { profilePic } = useStoreContext()
+  const { profilePic, editProfilePic } = useStoreContext()
+
+  const handleUploadProfilePic = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.target.files) {
+      editProfilePic(
+        createProfilePic({
+          profilePicUri: URL.createObjectURL(event.target.files[0]),
+        })
+      )
+    }
+  }
+
   return (
-    <Box position="relative">
+    <Box position="relative" width="100%">
       <Image
         boxSize="100%"
         src={profilePic.profilePicUri}
@@ -14,9 +28,17 @@ export const ProfilePicture: React.FC = () => {
       />
       <Flex position="absolute" top="0" left="0" minWidth="100%" p={2}>
         <Spacer />
-        <Button colorScheme="purple">
+        <Button as="label" htmlFor="profilePic" colorScheme="purple">
           <EditIcon />
         </Button>
+        <Input
+          type="file"
+          name="profilePic"
+          id="profilePic"
+          accept="image/*"
+          hidden
+          onChange={handleUploadProfilePic}
+        />
       </Flex>
     </Box>
   )
