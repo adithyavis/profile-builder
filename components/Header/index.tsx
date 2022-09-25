e1e7b85d5718a199afd3a603ba97254ae0c47130
adithya_viswamithiran <adithya_viswamithiran@m-kiva.elecom>
2022-09-26 00:36:16 +0900

    Create Header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Center, Heading } from '@chakra-ui/react'
+
+export const Header: React.FC = () => {
+  return (
+    <Center h="100%">
+      <Heading as="h2">Profile builder</Heading>
+    </Center>
+  )
+}
