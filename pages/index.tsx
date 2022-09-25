4046e926ac7a83eefbee82aa89522aaeac1a4a79
adithya_viswamithiran <adithya_viswamithiran@m-kiva.elecom>
2022-09-26 02:01:10 +0900

    final touch up and make page responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,43 @@
 import type { NextPage } from 'next'
+import { Grid, GridItem, VStack } from '@chakra-ui/react'
+import { useMediaQuery } from 'providers/MediaQueryProvider'
+import { Header } from 'components/Header'
+import { About } from 'components/About'
+import { Experiences } from 'components/Experiences'
 
 const Home: NextPage = () => {
-  return <></>
+  const { isMobile } = useMediaQuery()
+  if (isMobile) {
+    return (
+      <VStack>
+        <Header />
+        <About />
+        <Experiences />
+      </VStack>
+    )
+  }
+  return (
+    <Grid
+      templateAreas={`"header header"
+                  "sidebar main"`}
+      gridTemplateRows={'50px 1fr'}
+      gridTemplateColumns={'400px 1fr'}
+      h="100vh"
+      gap="1"
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <GridItem area={'header'}>
+        <Header />
+      </GridItem>
+      <GridItem area={'sidebar'}>
+        <About />
+      </GridItem>
+      <GridItem area={'main'} overflow="scroll">
+        <Experiences />
+      </GridItem>
+    </Grid>
+  )
 }
 
 export default Home
