26d1d631831f70ff750dd98a0c422158894ca029
adithya_viswamithiran <adithya_viswamithiran@m-kiva.elecom>
2022-09-26 00:30:08 +0900

    add methods to Store Provider

--- providers/StoreProvider.tsx
+++ providers/StoreProvider.tsx
@@ -1,33 +1,88 @@
-import React, { useContext, createContext } from 'react'
+import React, { useState, useContext, createContext } from 'react'
 
 import { ProfilePic } from 'types/ProfilePic'
 import { PersonalInfo } from 'types/PersonalInfo'
-import { Experience } from 'types/Experience'
+import { Experience, Id } from 'types/Experience'
 
-import { createProfilePic } from 'domain/ProfilePic'
-import { createPersonalInfo } from 'domain/PersonalInfo'
+import {
+  createProfilePic,
+  editProfilePic as _editProfilePic,
+} from 'domain/ProfilePic'
+import {
+  createPersonalInfo,
+  editPersonalInfo as _editPersonalInfo,
+} from 'domain/PersonalInfo'
+import {
+  createExperience,
+  addExperience as _addExperience,
+  editExperience as _editExperience,
+} from 'domain/Experience'
 
 type StoreContext = {
   profilePic: ProfilePic
   personalInfo: PersonalInfo | null
   experiences: Experience[]
+  addExperience: (args: Experience) => void
+  editExperience: (id: Id, args: Partial<Experience>) => void
+  editPersonalInfo: (args: PersonalInfo) => void
+  editProfilePic: (args: ProfilePic) => void
 }
 const StoreContext = createContext<StoreContext>({
   profilePic: createProfilePic(),
   personalInfo: createPersonalInfo(null),
   experiences: [],
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  addExperience: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  editExperience: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  editPersonalInfo: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  editProfilePic: () => {},
 })
 
 type StoreProvider = {
   children: React.ReactNode
 }
 export const StoreProvider: React.FC<StoreProvider> = ({ children }) => {
-  const profilePic: ProfilePic = createProfilePic()
-  const personalInfo: PersonalInfo | null = createPersonalInfo(null)
-  const experiences: Experience[] = []
+  const [profilePic, setProfilePic] = useState<StoreContext['profilePic']>(
+    createProfilePic()
+  )
+  const [personalInfo, setPersonalInfo] = useState<
+    StoreContext['personalInfo']
+  >(createPersonalInfo(null))
+  const [experiences, setExperiences] = useState<StoreContext['experiences']>([
+    createExperience(),
+  ])
+
+  const addExperience: StoreContext['addExperience'] = (args) => {
+    setExperiences(_addExperience(experiences, args))
+  }
+
+  const editExperience: StoreContext['editExperience'] = (id, args) => {
+    setExperiences(_editExperience(experiences, id, args))
+  }
+
+  const editPersonalInfo: StoreContext['editPersonalInfo'] = (args) => {
+    setPersonalInfo(_editPersonalInfo(personalInfo, args))
+  }
+
+  const editProfilePic: StoreContext['editProfilePic'] = (args) => {
+    setProfilePic(_editProfilePic(profilePic, args))
+  }
 
   return (
-    <StoreContext.Provider value={{ profilePic, personalInfo, experiences }}>
+    <StoreContext.Provider
+      value={{
+        profilePic,
+        personalInfo,
+        experiences,
+        addExperience,
+        editExperience,
+        editPersonalInfo,
+        editProfilePic,
+      }}
+    >
       {children}
     </StoreContext.Provider>
   )
